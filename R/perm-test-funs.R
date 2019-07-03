4f9851f9b03dc0f8b63268fa84e5d62223c4b5cf
Matthew T. Warkentin <matt.warkentin@mail.utoronto.ca>
2019-07-02 20:28:32 -0400

    completely working version of perms function

--- R/perm-test-funs.R
+++ R/perm-test-funs.R
@@ -1,58 +1,99 @@
 #' @title Permutation P-Values
 #'
-#' @description Set of functions for computing permutation-based P-values for any function. The syntax and usage of these functions are motivated by the `tidy eval` principles. This function allows for easy integration with `purrr::map` functions.
+#' @description A function for generating permuted datasets, where one can permute as many columns as desired. Stratified (i.e. group-based) shuffling can be achieved by specifying the `strata` argument. See details for a complete description.
 #'
 #' @param data A data frame.
 #' @param ... Variables in data frame to permute/shuffle.
+#' @param strata A discrete varible to perform stratified permutations.
 #' @param times Number of permutations.
-#' @param strata A discrete varible to perform stratified sampling and permutations.
-#' @param frac A fraction for up or downsampling your data. For `frac` greater than 1 you must also set `replace` to `TRUE` (i.e. upsample with replacement).
-#' @param replace A logical. Should sampling be done with replacement?
 #' @param apparent A logical. Should a copy of the input `data` be returned?
+#' @param seed A numeric value used to set the RNG seed for reproducible permutations.
 #'
-#' @return A data frame (`tibble`) where each row is a permuted version of the input data. The returned data frame has the added class `ptbl` which can be used for S3 generics.
+#' @details This function was motivated by the `rsample` package which allows straightforward implementation of several common resampling methods (e.g. boostrap, K-fold crossvalidation). While the internal mechanisms of this function are quite different, the goal is to provide a function that works like rsample for permuted data. This function works well with the pipe (`%>%`).
+#'
+#' After using `perms`, one can compute permutation-based P-values for using any function, including custom functions, in a concise manner. The syntax and usage of this function is motivated by the `tidy eval` principles. Thus, you specify both the names of the columns to permute and the stratitfying variable as bare column names, not quoted names. The default number of permutations is aligned with the default number of bootstraps for `rsample::bootstraps()`.
+#'
+#' This function allows for easy integration with `purrr::map` functions for functional programming. See the examples for a use-case. Also, consider the using `furrr` equivalents for parallel computations.
+#'
+#' @return A data frame (`tibble`) where each row is a permuted version of the input data. The returned data frame has the added class `perms` which can be used for S3 generic dispatch.
+#'
+#' @examples
+#'
+#' iris %>%
+#'   perms(Sepal.Length)
 #'
 #' @export
 
+perms <- function(data = NULL, ..., strata = NULL,
+                  times = 25, apparent = FALSE, seed = NULL) {
 
-perm2 <- function(data = NULL, ..., times = 25, strata = NULL,
-                  frac = 1.0, replace = FALSE, apparent = FALSE) {
+  vars <- dplyr::enquos(...)
+  strata_quo <- dplyr::enquo(strata)
+  strata_expr <- dplyr::enexpr(strata)
 
-  vars <- enquos(...)
+  if (!is.null(seed) & is.numeric(seed)){
+    seed
+  } else {
+    seed <- NULL
+    seed
+  }
 
-  assertthat::assert_that(class(data) %in% c('tbl', 'data.frame'))
+  set.seed(seed)
+
+  assertthat::assert_that(length(vars)>0,
+                          all(match(purrr::map(vars, rlang::get_expr),
+                                    names(data),
+                                    nomatch = FALSE)),
+                          msg = "One of the provided column names was not found in the data mask!")
+
+
+  assertthat::assert_that(any(class(data) %in% c('tbl', 'data.frame')))
   assertthat::assert_that(!is.na(as.integer(times)))
-  assertthat::assert_that(is.logical(replace))
   assertthat::assert_that(is.logical(apparent))
 
-  if (is.null(strata)) {
+  rec <- recipes::recipe(x = data) %>%
+    recipes::step_shuffle(!!!vars) %>%
+    recipes::prep(verbose = FALSE, retain = FALSE)
+
+  if (is.null(strata_expr)) {
     permd <- data %>%
-      dplyr::sample_frac(size = frac, replace = replace) %>%
       tidyr::crossing(., id = 1:times) %>%
       dplyr::group_nest(id) %>%
       dplyr::mutate(id = glue::glue('Permutation{stringr::str_pad(id,
                                   max(nchar(times)), pad = "0")}')) %>%
-      dplyr::select(id, dplyr::everything())
+      dplyr::mutate(data = purrr::map(data, ~recipes::bake(rec, new_data = .)))
 
-  } else if (match(as.character(strata), names(data))) {
-    data <- data %>%
-      group_by(!! strata) %>%
-      dplyr::sample_frac(size = frac, replace = replace) %>%
-      ungroup()
-
+  } else if (match(as.character(strata_expr), names(data))) {
+    permd <- data %>%
+      tidyr::crossing(., id = 1:times) %>%
+      dplyr::group_nest(id) %>%
+      dplyr::mutate(id = glue::glue('Permutation{stringr::str_pad(id,
+                                  max(nchar(times)), pad = "0")}')) %>%
+      mutate(data = purrr::map(data, ~strat_bake(., !!strata_quo, rec)))
 
   } else {
-    stop(glue::glue('Variable ({as.character(strata)}) must be a column in supplied data set!'))
+    stop(glue::glue('Variable ({as.character(strata_expr)}) must be a column in supplied data set!'))
   }
 
-  if (!apparent) {
+  class(permd) <- c(class(permd), 'perms')
+
+  suppressWarnings(if (!apparent) {
     return(permd)
   } else {
-    permd %>%
-      dplyr::bind_cols(.,
+    permd <- permd %>%
+      dplyr::bind_rows(.,
                        tidyr::nest(data) %>%
                          dplyr::mutate(id = 'Apparent')
                        )
     return(permd)
-  }
+  })
+}
+
+strat_bake <- function(data, strata, recipe){
+  strata <- enquo(strata)
+
+  data %>%
+    group_nest({{ strata }}, keep = TRUE) %>%
+    mutate({{ strata }} := purrr::map(data, ~bake(recipe, new_data = .))) %>%
+    unnest({{ strata }})
 }
