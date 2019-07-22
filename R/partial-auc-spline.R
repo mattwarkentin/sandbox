58ded209e76c9b4104e01bb8f592b561b137e8b4
Matthew T. Warkentin <matt.warkentin@mail.utoronto.ca>
2019-07-22 19:35:46 -0400

    Updated partAUC documentation; fixed smoothing code

--- R/partial-auc-spline.R
+++ R/partial-auc-spline.R
@@ -1,22 +1,22 @@
-#' @title Partial AUC
+#' @title Partial Area Under the Curve
 #'
-#' @description Function for estimating the partial area under the ROC curve. See details for a more comprehensive description.
+#' @description Function for estimating the partial area under the ROC curve. ROC curve may be optionally smoothed using binormal smoothing before computing partial AUC. See \code{Details} for a more comprehensive description.
 #'
 #' @param data Data frame.
-#' @param sens Column name for sensitivity.
-#' @param spec Column name for specificity.
+#' @param sens Column name in \code{data} for sensitivity.
+#' @param spec Column name in \code{data} for specificity.
 #' @param range Range of sensitivity or specificity to integrate.
 #' @param focus String describing whether the integration range is for sensitivity or specificity.
 #' @param n Number of equally-spaced points for smooth curve calculations.
 #' @param ... Not currently used.
 #'
-#' @return \code{Tibble} containing a column for the partial AUC, and a list-column containing the ROC data.
+#' @return Invisibly returns a \code{\link[tibble]{tibble}} containing a column for the partial AUC, and a list-column containing the ROC data. The returned \code{\link[tibble]{tibble}} inherits the \code{\link[tibble]{tibble}} classes as well as the class \code{partial_auc}. The returned object also contains attributes indicating whether the ROC was smoothed (\code{smooth_roc}), corrected (\code{corrected_auc}), range of integration (\code{range}), and focus (\code{focus}).
 #'
-#' @details Since the x-axis of the ROC curve is mapped as 1 - Specificity, if the \code{focus} is "sp", the range should be provided in the left-to-right orientation for specificity. In other words, if you want to integrate the entire ROC in terms of specificity, the range would be c(1,0); this translates to integrating the false-positive rate from c(0,1).
+#' @details Since the x-axis of the ROC curve is mapped as 1 - Specificity, if the \code{focus} is "sp", the \code{range} should be provided in the left-to-right orientation for specificity. In other words, if you want to integrate the entire ROC in terms of specificity, the \code{range} would be c(1,0); this translates to integrating the false-positive rate from c(0,1).
 #'
-#' If you only want to integrate the a section of the ROC curve (e.g. FPR from 0 to 0.2), you would specify the range in terms of specificity as c(1, 0.8).
+#' If you only want to integrate the a section of the ROC curve (e.g. FPR from 0 to 0.2), you would specify the \code{range} in terms of specificity as c(1, 0.8).
 #'
-#' When the focus is "se" for sensitivity, the range should be specified from the bottom-to-top (standard y-axis) orientation. In other words, to integrate the whole ROC curve, if the focus is "se" the range should be specificed as c(0,1). To integrate only a section of the ROC, for example, when sensitivity is between 0 and 0.5, you would specify the focus as "se" and the range as c(0,0.5).
+#' When the \code{focus} is "se" for sensitivity, the \code{range} should be specified from the top-to-bottom orientation. In other words, to integrate the whole ROC curve, if the \code{focus} is "se" the \code{range} should be specificed as c(1,0). To integrate only a section of the ROC, for example, when sensitivity is between 0 and 0.5, you would specify the \code{focus} as "se" and the \code{range} as c(0.5,0).
 #'
 #' @examples
 #' dd <- tibble::tibble(sens = runif(100), spec = runif(100))
@@ -42,8 +42,8 @@ partAUC <- function(data = NULL, sens = NULL, spec = NULL,
 
 
   data <- data %>%
-    transmute(sens = {{ sens }},
-           spec = {{ spec }})
+    dplyr::transmute(sens = {{ sens }},
+              spec = {{ spec }})
 
   if (smooth) {
     data2 <- data %>% dplyr::mutate_all(qnorm)
@@ -52,11 +52,11 @@ partAUC <- function(data = NULL, sens = NULL, spec = NULL,
 
     lin.mod <- lm(spec ~ sens, data2)
 
-    se <- qnorm(seq(0, 1, 1 / (n - 1)))
-    sp <- predict(lin.mod, newdata = data.frame(se))
+    sens <- qnorm(seq(0, 1, 1 / (n - 1)))
+    spec <- predict(lin.mod, newdata = as.data.frame(sens))
 
-    data <- tibble::tibble(sens = pnorm(se),
-                           spec = pnorm(sp))
+    data <- tibble::tibble(sens = pnorm(sens),
+                           spec = pnorm(spec))
     coefs <- coefficients(lin.mod)
     smooth_auc <- unname(pnorm(coefs[1]/sqrt(1 + coefs[2]^2)))
     smoothed <- "Binormal Smoothed "
@@ -82,7 +82,7 @@ partAUC <- function(data = NULL, sens = NULL, spec = NULL,
   cat(glue::glue('{smoothed}Partial AUC ({focus} {range[1]}-{range[2]}): {round(p_auc, 4)} {corrected}\n'))
 
 
-  x <- bind_cols(auc = p_auc, nest(data, .key = 'roc'))
+  x <- dplyr::bind_cols(auc = p_auc, tidyr::nest(data, .key = 'roc'))
 
   attr(x, "smooth_roc") <- smooth
   attr(x, "corrected_auc") <- correct
