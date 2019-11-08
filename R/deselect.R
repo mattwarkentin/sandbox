cec66823598e6680b1fe8682b60a8271708255ea
Matthew T. Warkentin <matt.warkentin@mail.utoronto.ca>
2019-11-08 14:58:48 -0500

    added deselect function

---
+++ R/deselect.R
@@ -0,0 +1,23 @@
+#' Deselect variables by name
+#'
+#' @description Choose variables from a tbl. deselect() drops only the variables you mention. Supports \code{tidyselect::select_helpers}
+#'
+#' @param .data Data frame.
+#' @param ... One or more unquoted expressions separated by commas. You can treat variable names like they are positions, so you can use expressions like x:y to select ranges of variables.
+#'
+#' Positive values drop variables; negative values keep variables.
+#'
+#' @return An object of the same class as .data.
+#' @export
+#'
+#' @examples
+#'
+#' iris %>%
+#'   deselect(Species)
+#'
+deselect <- function(.data, ...){
+  vars <- enquos(...)
+
+  .data %>%
+    select(-(!!!vars))
+}
